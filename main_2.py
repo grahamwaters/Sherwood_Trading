2195363c0bb0912c14c4feb859881e1a70e27c68
graham <graham.waters.business@gmail.com>
2023-07-31 13:49:11 -0500

    working mostly One error with sellcryptolimit

--- main_2.py
+++ main_2.py
@@ -6,9 +6,37 @@ from robin_stocks import robinhood as r
 from datetime import datetime
 from pytz import timezone
 import asyncio
-from legacy.V5.main2 import stop_loss_percent
+import time
+import os
 from tqdm import tqdm
 from colorama import Fore, Back, Style
+from icecream import ic
+import configparser
+
+#^ load the relevant variables from the .ini credentials file in config/
+# [trading]
+# coins = BTC, ETH, DOGE, SHIB, ETC, UNI, AAVE, LTC, LINK, COMP, AVAX, XLM, BCH, XTZ
+# stop_loss_percent = 0.05
+# percent_to_use = 0.8
+
+# [logging]
+# verbose_mode = True
+# debug_verbose = True
+# reset_positions = False
+
+config = configparser.ConfigParser()
+config.read('config/credentials.ini')
+coins = config['trading']['coins'].split(', ')
+stop_loss_percent = float(config['trading']['stop_loss_percent'])
+coins = [coin.strip() for coin in coins]
+percent_to_use = float(config['trading']['percent_to_use'])
+verbose_mode = config['logging']['verbose_mode']
+debug_verbose = config['logging']['debug_verbose']
+reset_positions = config['logging']['reset_positions']
+minimum_usd_per_position = float(config['trading']['minimum_usd_per_position']) #note: this is the minimum amount of USD that must be invested at any given time in each position. All trades must keep this in mind.
+# initialize the Trader class
+
+
 class Utility:
     def __init__(self):
         """
@@ -105,11 +133,15 @@ class Trader:
         self.logger.addHandler(handler)
         # Login to Robinhood
         self.login_setup()
+        # self.utility_object = Utility()
+        # self.log_file_size_checker = self.utility_object.log_file_size_checker() #note: this should be causing an error because of asyncio
+        # self.loop = asyncio.get_event_loop()
     def login_setup(self):
         """
         The login_setup function logs into Robinhood using the provided username and password.
         :doc-author: Trelent
         """
+        ic()
         try:
             r.login(self.username, self.password)
             self.logger.info('Logged in to Robinhood successfully.')
@@ -120,6 +152,7 @@ class Trader:
         The resetter function cancels all open orders and sells all positions.
         :doc-author: Trelent
         """
+        ic()
         try:
             open_orders = r.get_all_open_crypto_orders()
             print(Fore.YELLOW + 'Canceling all open orders...' + Style.RESET_ALL)
@@ -129,7 +162,14 @@ class Trader:
             self.logger.info('All open orders cancelled.' + Style.RESET_ALL)
             crypto_positions = r.get_crypto_positions()
             for position in crypto_positions:
-                r.order_sell_crypto_limit(position['currency']['code'], position['quantity'], position['cost_bases'][0]['direct_cost_basis'])
+                #note: keep at least minimum_usd_per_position in each position (USD) value not coin value.
+                if float(position['quantity']) * float(position['cost_bases'][0]['direct_cost_basis']) < minimum_usd_per_position: # if position is less than minimum_usd_per_position
+                    # then skip it
+                    continue
+                else:
+                    ic()
+                    r.order_sell_crypto_limit(position['currency']['code'], position['quantity'], position['cost_bases'][0]['direct_cost_basis'])
+                    self.logger.info(f'Sold {position["quantity"]} {position["currency"]["code"]}.')
             self.logger.info('All positions sold.')
         except Exception as e:
             self.logger.error(f'Unable to reset orders and positions... {e}')
@@ -140,25 +180,50 @@ class Trader:
         :param coins: A list of coins to generate signals for
         :return: A DataFrame with the trading signals for each coin
         """
+        print(Fore.YELLOW + 'Calculating technical indicators...' + Style.RESET_ALL)
+        ic()
         try:
             utility = Utility()
             signals_df = pd.DataFrame()
             for coin in coins:
+                # ic()
                 df = utility.get_last_100_days(coin)
+                df['coin'] = coin #^ add coin name to df
                 df['sma'] = df.close.rolling(window=50).mean()
                 df['ema'] = df.close.ewm(span=50, adjust=False).mean()
                 df['macd_line'], df['signal_line'], df['macd_hist'] = ta.macd(df.close)
                 df['rsi'] = ta.rsi(df.close)
-                df['williams'] = ta.williams_r(df.high, df.low, df.close)
-                df['stochastic_k'], df['stochastic_d'] = ta.stoch(df.high, df.low, df.close)
-                df['bollinger_l'], df['bollinger_m'], df['bollinger_u'] = ta.bollinger_bands(df.close)
+                #^ Note: The code below fixes errors in the tas
+                df['williams'] = ta.willr(df.high, df.low, df.close) #^ Fixes error in williams_r
+                df['stochastic_k'], df['stochastic_d'] = ta.stoch(df.high, df.low, df.close) #^ Fixes error in stoch
+
+                # ['BBL_5_2.0', 'BBM_5_2.0', 'BBU_5_2.0', 'BBB_5_2.0', 'BBP_5_2.0']
+                # the above are the column names for the bollinger bands, these must be put into df as columns on the left of the "=" sign then self.bollinger(df) must be put on the right of the "=" sign
+                df['bollinger_l'], df['bollinger_m'], df['bollinger_u'], df['bollinger_b'], df['BBP_5_2.0'] = ta.bbands(df.close, length=5, std=2) #^ Fixes error in bollinger_bands (length and std must be specified)
+                # df['bollinger_l'], df['bollinger_m'], df['bollinger_u'] = self.bollinger(df) #^ Fixes error in bollinger_bands
+
+
+
                 df['buy_signal'] = ((df.macd_line > df.signal_line) & (df.rsi < 30)) | ((df.stochastic_k > df.stochastic_d) & (df.williams < -80))
                 df['sell_signal'] = ((df.macd_line < df.signal_line) & (df.rsi > 70)) | ((df.stochastic_k < df.stochastic_d) & (df.williams > -20))
+
+
+
+
+
+
+
+
+                print(f'Buy signal for {coin}: {df.buy_signal.iloc[-1]}')
+                print(f'Sell signal for {coin}: {df.sell_signal.iloc[-1]}')
                 signals_df = signals_df.append(df)
             return signals_df
         except Exception as e:
             self.logger.error(f'Unable to generate trading signals... {e}')
             return pd.DataFrame()
+
+
+
     def trading_function(self, signals_df):
         """
         The trading_function function takes the trading signals generated by calculate_ta_indicators() and places trades accordingly.
@@ -166,47 +231,29 @@ class Trader:
         :doc-author: Trelent
         """
         try:
+
             crypto_positions = r.get_crypto_positions()
+            cash_available = float(r.load_account_profile()['crypto_buying_power'])
             for index, row in signals_df.iterrows():
                 if row['buy_signal']:
-                    #* Create a nice little data viz block for the terminal that shows the TA indicators for why this position was bought
-                    block_text = f"""
-                    {row['coin']} bought at {row['close']} because:
-                    - MACD Line: {row['macd_line']}
-                    - Signal Line: {row['signal_line']}
-                    - RSI: {row['rsi']}
-                    - Williams %R: {row['williams']}
-                    - Stochastic K: {row['stochastic_k']}
-                    - Stochastic D: {row['stochastic_d']}
-                    """
-                    print(Fore.GREEN + block_text + Style.RESET_ALL)
-                    # Check if we have enough buying power to buy this coin
                     buying_power = self.update_buying_power()
                     if buying_power > 0:
-                        r.order_buy_crypto_limit(symbol=row['coin'],
+                        r.order.buy_crypto_limit(symbol=row['coin'],
                                                     quantity = buying_power / row['close'],
                                                     limitPrice = row['close'],
                                                     timeInForce = 'gtc')
                         self.logger.info(f'Bought {row["coin"]} at {row["close"]}.')
+                        buying_power -= float(cash_available)
                 if row['sell_signal']:
                     for position in crypto_positions:
-                        #* Create a nice little data viz block for the terminal that shows the TA indicators for why this position was sold
-                        block_text = f"""
-                        {row['coin']} sold at {row['close']} because:
-                        - MACD Line: {row['macd_line']}
-                        - Signal Line: {row['signal_line']}
-                        - RSI: {row['rsi']}
-                        - Williams %R: {row['williams']}
-                        - Stochastic K: {row['stochastic_k']}
-                        - Stochastic D: {row['stochastic_d']}
-                        """
-                        print(Fore.RED + block_text + Style.RESET_ALL)
                         if position['currency']['code'] == row['coin']:
-                            r.order_sell_crypto_limit(symbol=row['coin'],
-                                                        quantity=position['quantity'],
-                                                        limitPrice=row['close'],
-                                                        timeInForce='gtc')
+                            ic()
+                            r.order.sell_crypto_limit(symbol=row['coin'],
+                                                        quantity = position['quantity'],
+                                                        limitPrice = row['close'],
+                                                        timeInForce = 'gtc')
                             self.logger.info(f'Sold {row["coin"]} at {row["close"]}.')
+
         except Exception as e:
             self.logger.error(f'Unable to execute trades... {e}')
     def get_total_crypto_dollars(self):
@@ -233,8 +280,9 @@ class Trader:
         try:
             profile_info = r.load_account_profile()
             cash_available = float(profile_info['cash_available_for_withdrawal'])
-            crypto_dollars = self.get_total_crypto_dollars()
-            buying_power = cash_available + crypto_dollars
+            # crypto_dollars = self.get_total_crypto_dollars()
+            # buying_power = cash_available + crypto_dollars
+            buying_power = cash_available
             return buying_power
         except Exception as e:
             self.logger.error(f'Unable to update buying power... {e}')
@@ -246,6 +294,7 @@ class Trader:
         :param stop_loss_prices: A dictionary with the stop loss price for each coin
         :doc-author: Trelent
         """
+        ic()
         try:
             for coin in tqdm(coins):
                 current_price = float(r.crypto.get_crypto_quote(coin)['mark_price'])
@@ -253,7 +302,10 @@ class Trader:
                     crypto_positions = r.get_crypto_positions()
                     for position in crypto_positions:
                         if position['currency']['code'] == coin:
-                            r.order_sell_crypto_limit(coin, position['quantity'], current_price)
+                            r.order.sell_crypto_limit(symbol=coin,
+                                                        quantity=position['quantity'],
+                                                        limitPrice=current_price,
+                                                        timeInForce='gtc')
                             self.logger.info(f'Sold {coin} at {current_price} due to stop loss.')
         except Exception as e:
             self.logger.error(f'Unable to check stop loss prices... {e}')
@@ -307,6 +359,7 @@ class Looper:
         """
         try:
             if loop_count % 10 == 0:
+                # update buying power every 10 loops
                 self.trader.update_buying_power()
             self.trader.main(coins, stop_loss_prices)
             # run all async functions simultaneously
@@ -334,11 +387,10 @@ class Looper:
                 self.logger.error(f'Error in main loop... {e}')
 # run the program
 if __name__ == '__main__':
-    stop_loss_percent = 0.05 #^ set the stop loss percent at 5% (of the invested amount)
-    coins = ['BTC', 'ETH', 'DOGE', 'SHIB', 'ETC', 'UNI', 'AAVE', 'LTC', 'LINK', 'COMP', 'USDC', 'AVAX', 'XLM', 'BCH', 'XTZ']
     #^ set stop losses for each coin by multiplying the current price by the stop loss percent (0.05) and subtracting that from the current price (to get the stop loss price).
+    trader = Trader(username= config['robinhood']['username'],
+                    password= config['robinhood']['password']) #^ create an instance of the Trader class, and log in with the config's username and password
     stop_loss_prices = {coin: float(r.crypto.get_crypto_quote(coin)['mark_price']) - (float(r.crypto.get_crypto_quote(coin)['mark_price']) * stop_loss_percent) for coin in coins}
     print(f'Stop loss prices: {stop_loss_prices}')
-    trader = Trader() #^ create an instance of the Trader class
     looper = Looper(trader) #^ create an instance of the Looper class (which will run the Trader class)
     asyncio.run(looper.main_looper(coins, stop_loss_prices)) #^ run the main_looper function
